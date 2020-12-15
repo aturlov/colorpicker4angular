1fccef37362d036a57969ae1e13ab020db9a6ab1
Alexander Turlov <aturlov@objectsharp.com>
2020-12-14 20:21:10 -0500

    Empty project created

---
+++ ColorPicker4Angular/projects/apt-color-picker/src/lib/apt-color-picker.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'apt-ColorPicker',
+  template: `
+    <p>
+      apt-color-picker works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class AptColorPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ColorPicker4Angular/projects/apt-color-picker/src/lib/apt-color-picker.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AptColorPickerComponent } from './apt-color-picker.component';
+
+
+
+@NgModule({
+  declarations: [AptColorPickerComponent],
+  imports: [
+  ],
+  exports: [AptColorPickerComponent]
+})
+export class AptColorPickerModule { }
