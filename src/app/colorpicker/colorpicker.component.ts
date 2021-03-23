db44559a18ad42d3a7f73725f6fb08a7fc121e0f
Grigory Koftaylov <koftaylov@gmail.com>
2021-03-23 18:35:40 +0000

    dame some make up, no critical

--- src/app/colorpicker/colorpicker.component.ts
+++ src/app/colorpicker/colorpicker.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class ColorpickerComponent implements OnInit {
   flagVisibleTable:boolean = false;
   checkedColor:string = "";
-  constructor() { }
+  numbers?:number[];
+  constructor() {
+    this.numbers = Array(5).fill(1).map((x,i)=>i+1); // [0,1,2,3,4]
+    //this.numbers = Array(5).fill(4); // [4,4,4,4,4] 
+  }
 
   ngOnInit(): void {
   }
@@ -19,9 +23,9 @@ export class ColorpickerComponent implements OnInit {
   }
 
 
-  onCheckColor():void{
+  onCheckColor(i:number,j:number):void{
     this.flagVisibleTable = !(this.flagVisibleTable);
-    this.checkedColor = "Color was checked"
+    this.checkedColor = `Color was checked ${i}${j}`
   }
 
 }
