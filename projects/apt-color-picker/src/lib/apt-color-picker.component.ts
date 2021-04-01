3fa80dbb65ec2808765a1f07176531e66e9e8261
Alexander Turlov <aturlov@gmail.com>
2021-04-01 11:42:53 -0400

    Added AptColorPicker interface with TODO

--- projects/apt-color-picker/src/lib/apt-color-picker.component.ts
+++ projects/apt-color-picker/src/lib/apt-color-picker.component.ts
@@ -1,12 +1,19 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ColorChangeEvent } from './color-change-event';
 
+// TODO: refactor the rest of the code to use this interface instead of AptColorPickerComponent
+export interface AptColorPicker {
+  palette: string[];
+  selectedColorChange: EventEmitter<ColorChangeEvent>;
+  selectedColor:string;
+}
+
 @Component({
   selector: 'apt-ColorPicker',
   templateUrl:'./apt-color-picker.component.html' ,
   styleUrls:['./apt-color-picker.component.css']
 })
-export class AptColorPickerComponent implements OnInit {
+export class AptColorPickerComponent implements AptColorPicker, OnInit {
   
   private _selectedColor: string = "";
   
