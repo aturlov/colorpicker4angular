9f00f4c3597beaf5ee48b2e9d6158d89f528c767
Gr.Koftaylov <koftaylov@gmail.com>
2021-04-03 02:31:55 +0100

    refactored code to use interface AptColorPicker (#20)

--- projects/apt-color-picker/src/lib/color-change-event.ts
+++ projects/apt-color-picker/src/lib/color-change-event.ts
@@ -1,6 +1,6 @@
-import { AptColorPickerComponent } from "./apt-color-picker.component";
+import { AptColorPicker } from "./apt-color-picker.component";
 
 export interface ColorChangeEvent {
     color: string;
-    target: AptColorPickerComponent;
+    target: AptColorPicker;
 }
